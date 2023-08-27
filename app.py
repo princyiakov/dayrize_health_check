17f59dbd53e72ac03ffd21f0eef700ace29a1300
princyiakov <princy@giskard.ai>
2023-08-28 00:09:50 +0800

    app with columns names updated along with overall visualisation

--- app.py
+++ app.py
@@ -1,108 +1,10 @@
 import streamlit as st
 from src.components.data_health_check import DataHeathCheck
 import plotly.express as px
-#
-# def main():
-#     st.title("Data Health Check")
-#
-#     uploaded_file = st.file_uploader("Upload your excel file", type=["xlsx"])
-#
-#     if uploaded_file is not None:
-#         dhc = DataHeathCheck(uploaded_file)
-#         st.success("File Uploaded Successfully")
-#
-#         st.write("Redirecting to Visualisation")
-#         data = dhc.calculate_health_check()
-#         show_visualisation(data)
-#     else:
-#         st.info("Please upload a Excel File to proceed")
-#
-#
-# def show_visualisation(data):
-#     st.title("Data Visualisation")
-#
-#     # Create a dropdown widget for selecting a value from the "Name" column
-#     selected_name = st.sidebar.selectbox("Select a Product Name", data["Name"].unique())
-#
-#     # Select Dataframe
-#     selected_data = data[data["Name"] == selected_name]
-#     count_df = selected_data[["primary_data_count", "missing_data_count", "proxy_data_count"]]
-#     # Define custom colors for each section
-#     custom_colors = {
-#         "primary_data_count": "green",
-#         "missing_data_count": "red",
-#         "proxy_data_count": "yellow"
-#     }
-#     # Create Pie chart
-#     st.subheader("Pie Chart for Product Name : ", selected_name)
-#     fig = px.pie(values=[selected_data.primary_data_count.iloc[0], selected_data.missing_data_count.iloc[0],
-#                          selected_data.proxy_data_count.iloc[0]],
-#                  names=["primary_data_count", "missing_data_count", "proxy_data_count"],
-#                  color_discrete_map=custom_colors)
-#     # fig = px.pie(count_df,
-#     #              values=count_df.values,
-#     #              names=count_df.index
-#     #              )
-#     #st.plotly_chart(fig, use_container_width=True)
-#     st.write(fig)
-#     # st.markdown(
-#     #     f'<div style="background-color: red; padding: 10px; border-radius: 10px;">'
-#     #     f'<h2 style="color: white;">{str(selected_data["missing_data_columns"])}</h2>'
-#     #     f'<p style="color: white;">This is some information about {selected_data["missing_data_columns"]}</p>'
-#     #     '</div>',
-#     #     unsafe_allow_html=True
-#     # )
-#
-# # import streamlit as st
-# # import random
-# #
-# # # Function to generate a random color
-# # def get_random_color():
-# #     color = "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-# #     return color
-# #
-# # def main():
-# #     st.title("List of Names with Colorful Blocks")
-# #
-# #     names = [
-# #         "John Doe",
-# #         "Jane Smith",
-# #         "Michael Johnson",
-# #         "Emily Brown",
-# #         "William Davis"
-# #     ]
-# #
-# #     for name in names:
-# #         # Generate a random background color for each name block
-# #         background_color = get_random_color()
-# #
-# #         # Apply the color to a styled div
-# #         st.markdown(
-# #             f'<div style="background-color: red; padding: 10px; border-radius: 10px;">'
-# #             f'<h2 style="color: white;">{name}</h2>'
-# #             f'<p style="color: white;">This is some information about {name}</p>'
-# #             '</div>',
-# #             unsafe_allow_html=True
-# #         )
-# #
-# if __name__ == "__main__":
-#     main()
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
-
-# # Sample data for demonstration
-# data = pd.DataFrame({
-#     "Name": ["Product A", "Product B", "Product C"],
-#     "primary_data_count": [100, 150, 200],
-#     "missing_data_count": [10, 20, 30],
-#     "proxy_data_count": [5, 10, 15]
-# })
 
 
 def main():
-    st.title("Data Visualization App")
+    st.title("Health Check App")
     st.title("Upload File")
     uploaded_file = st.file_uploader("Upload your excel file", type=["xlsx"])
 
@@ -110,16 +12,18 @@ def main():
         st.success("File Uploaded Successfully")
         # Display the Upload File section
         dhc = DataHeathCheck(uploaded_file)
-        data = dhc.calculate_health_check()
 
         # Create tabs for navigation between Individual Records and Overall Records
-        tabs = ["Individual Records", "Overall Records"]
+        tabs = ["Overall Records", "Individual Records"]
         active_tab = st.radio("Select a page", tabs)
 
-        if active_tab == "Individual Records":
+        if active_tab == "Overall Records":
+            comparison_data = dhc.create_comparison_df()
+            visualise_overall_records(comparison_data)
+        elif active_tab == "Individual Records":
+            data = dhc.calculate_health_check()
+
             individual_records_page(data)
-        elif active_tab == "Overall Records":
-            overall_records_page(data)
 
     else:
         st.info("Please upload a Excel File to proceed")
@@ -132,29 +36,68 @@ def individual_records_page(data):
     selected_data = data[data["Name"] == selected_name]
 
     st.subheader(f"Visualization for Product Name: {selected_name}")
-    visualize_data(selected_data)
+    visualize_single_data(selected_data)
 
 
-def overall_records_page(data):
+def visualise_overall_records(data):
     st.title("Overall Records Visualization")
-    visualize_data(data)
+    fig = px.bar(data,
+                 orientation='h',
+                 labels={'index': 'Columns'},
+                 title='Null Value Comparison',
+                 template='plotly_dark')
+
+    fig.update_layout(barmode='stack')
+    fig.update_layout(
+        barmode='stack',
+        height=1000,  # Adjust the height
+        width=1000  # Adjust the width
+    )
+    st.plotly_chart(fig, use_container_width=True)
 
 
-def visualize_data(data):
-    # Define custom colors for each section
+def visualize_single_data(data):
+    map_colours = {"primary_data_count": "green",
+                   "missing_data_count": "crimson",
+                   "proxy_data_count": "orange"
+                   }
+    labels = ["primary_data_count", "missing_data_count", "proxy_data_count"]
 
     # Create Pie chart
     fig = px.pie(values=[data.primary_data_count.iloc[0], data.missing_data_count.iloc[0],
                          data.proxy_data_count.iloc[0]],
-                 names=["primary_data_count", "missing_data_count", "proxy_data_count"],
-                 color=["primary_data_count", "missing_data_count", "proxy_data_count"],
-                 color_discrete_map={
-                     "primary_data_count": "green",
-                     "missing_data_count": "crimson",
-                     "proxy_data_count": "orange"
-                 })
+                 names=labels,
+                 color=labels,
+                 color_discrete_map=map_colours)
 
     st.plotly_chart(fig, use_container_width=True)
+
+    primary_columns = ", ".join(list(data["primary_data_columns"].iloc[0]))
+    st.markdown(
+        f'<div style="background-color: green; padding: 10px; border-radius: 10px;">'
+        f'<h3 style="color: white;"> Primary Data Columns provided by User</h3>'
+        f'<p style="color: white;">{primary_columns}</p>'
+        '</div>',
+        unsafe_allow_html=True
+    )
+
+    proxy_columns = ", ".join(list(data["proxy_data_columns"].iloc[0]))
+    st.markdown(
+        f'<div style="background-color: orange; padding: 10px; border-radius: 10px;">'
+        f'<h3 style="color: white;"> Proxy Data Columns filled by Dayrize </h3>'
+        f'<p style="color: white;">{proxy_columns}</p>'
+        '</div>',
+        unsafe_allow_html=True
+    )
+
+    missing_columns = ", ".join(list(data["missing_data_columns"].iloc[0]))
+    st.markdown(
+        f'<div style="background-color: red; padding: 10px; border-radius: 10px;">'
+        f'<h3 style="color: white;"> Missing Data Columns</h3>'
+        f'<p style="color: white;">{missing_columns}</p>'
+        '</div>',
+        unsafe_allow_html=True
+    )
 
 
 if __name__ == "__main__":
--- src/components/data_health_check.py
+++ src/components/data_health_check.py
@@ -13,6 +13,9 @@ class DataHeathCheck:
         assert self.original_data['GTIN'].equals(self.processed_data['GTIN']), "Original Data GTIN values are not " \
                                                                                "same as processed values"
 
+    def get_data(self):
+        return self.original_data, self.processed_data
+
     # Define a function to get non-null column names for a row
     def get_non_null_columns(self, row):
         return set(row.index[row.notna()])
@@ -33,9 +36,30 @@ class DataHeathCheck:
                                                   - self.processed_data['primary_data_count'] \
                                                   - self.processed_data['missing_data_count']
         # Apply the function to each row
-        self.processed_data['primary_data_columns'] = self.original_data.apply(self.get_non_null_columns, axis=1)
-        self.processed_data['missing_data_columns'] = self.original_data.apply(self.get_null_columns, axis=1)
-        self.processed_data['proxy_columns'] = set(self.columns_to_include) - self.processed_data[
+        self.processed_data['primary_data_columns'] = self.original_data[self.columns_to_include].apply(self.get_non_null_columns, axis=1)
+        self.processed_data['missing_data_columns'] = self.processed_data[self.columns_to_include].apply(self.get_null_columns, axis=1)
+        self.processed_data['proxy_data_columns'] = set(self.columns_to_include) - self.processed_data[
             'primary_data_columns'] - self.processed_data['missing_data_columns']
 
         return self.processed_data
+
+    def create_comparison_df(self):
+        # Calculate the percentage of null values for both DataFrames
+        self._verify_data()
+        org_data_null_percent = (self.original_data.isnull().sum() / len(self.original_data)) * 100
+        processed_data_null_percent = (self.processed_data.isnull().sum() / len(self.processed_data)) * 100
+
+        # Create a new DataFrame to store the null value percentages
+        comparison_df = pd.DataFrame({
+            # 'Columns': org_data_null_percent.index,
+            'org_data_null_percent': org_data_null_percent,
+            'processed_data_null_percent': processed_data_null_percent,
+            # 'total_null' : org_data_null_percent + processed_data_null_percent
+        })
+        comparison_df.fillna(0, inplace=True)
+        comparison_df['total_null'] = comparison_df['org_data_null_percent'] + comparison_df[
+            'processed_data_null_percent']
+        comparison_df = comparison_df.sort_values(by='total_null', ascending=False)
+        comparison_df.drop('total_null', inplace=True, axis=1)
+
+        return comparison_df
