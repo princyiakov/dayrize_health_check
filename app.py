a2cd53e4297f18efbbe3ca8666993bba9ff56852
princyiakov <princy@giskard.ai>
2023-08-28 23:15:48 +0800

    comments added

--- app.py
+++ app.py
@@ -1,52 +1,62 @@
+# Import necessary libraries
 import streamlit as st
-from src.components.data_health_check import DataHeathCheck
+from src.components.data_health_check import DataHeathCheck  # Importing a custom module
 import plotly.express as px
 
 
+# Define the main function
 def main():
+    # Set the title of the app
     st.title("Health Check App")
-    st.title("Upload File")
+    st.title("Upload File")  # Title for the file upload section
+
+    # Create a file uploader for Excel files
     uploaded_file = st.file_uploader("Upload your excel file", type=["xlsx"])
 
     if uploaded_file is not None:
-        st.success("File Uploaded Successfully")
-        # Display the Upload File section
+        st.success("File Uploaded Successfully")  # Display a success message
+
+        # Initialize DataHealthCheck instance with the uploaded file
         dhc = DataHeathCheck(uploaded_file)
 
-        # Create tabs for navigation between Individual Records and Overall Records
+        # Create tabs for navigation between different sections
         tabs = ["Overall Records", "Individual Records"]
-        active_tab = st.radio("Select a page", tabs)
+        active_tab = st.radio("Select a page", tabs)  # Radio buttons for tab selection
 
         if active_tab == "Overall Records":
+            # Calculate and display comparison data for overall records
             comparison_data = dhc.create_comparison_df()
             visualise_overall_records(comparison_data)
         elif active_tab == "Individual Records":
+            # Calculate health check data for individual records and display it
             data = dhc.calculate_health_check()
-
             individual_records_page(data)
-
     else:
-        st.info("Please upload a Excel File to proceed")
+        st.info("Please upload an Excel File to proceed")  # Display an info message
 
 
+# Function to display individual records page
 def individual_records_page(data):
-    st.title("Individual Records Visualization")
+    st.title("Individual Records Visualization")  # Title for individual records page
 
+    # Dropdown to select a product name
     selected_name = st.selectbox("Select a Product Name", data["Name"].unique())
     selected_data = data[data["Name"] == selected_name]
 
-    st.subheader(f"Visualization for Product Name: {selected_name}")
+    st.subheader(f"Visualization for Product Name: {selected_name}")  # Subheader with selected product name
     visualize_single_data(selected_data)
 
 
+# Function to visualize overall records
 def visualise_overall_records(data):
     st.title("Overall Records Visualization")
+    st.title("Null Value Comparison")  # Title for null value comparison section
+
+    # Create a horizontal bar chart using Plotly Express
     fig = px.bar(data,
                  orientation='h',
                  labels={'index': 'Columns'},
-                 title='Null Value Comparison',
                  template='plotly_dark')
-
     fig.update_layout(barmode='stack')
     fig.update_layout(
         barmode='stack',
@@ -56,6 +66,7 @@ def visualise_overall_records(data):
     st.plotly_chart(fig, use_container_width=True)
 
 
+# Function to visualize individual record data
 def visualize_single_data(data):
     map_colours = {"primary_data_count": "green",
                    "missing_data_count": "crimson",
@@ -63,15 +74,15 @@ def visualize_single_data(data):
                    }
     labels = ["primary_data_count", "missing_data_count", "proxy_data_count"]
 
-    # Create Pie chart
+    # Create a pie chart using Plotly Express
     fig = px.pie(values=[data.primary_data_count.iloc[0], data.missing_data_count.iloc[0],
                          data.proxy_data_count.iloc[0]],
                  names=labels,
                  color=labels,
                  color_discrete_map=map_colours)
-
     st.plotly_chart(fig, use_container_width=True)
 
+    # Display primary data columns
     primary_columns = ", ".join(list(data["primary_data_columns"].iloc[0]))
     st.markdown(
         f'<div style="background-color: green; padding: 10px; border-radius: 10px;">'
@@ -81,6 +92,7 @@ def visualize_single_data(data):
         unsafe_allow_html=True
     )
 
+    # Display proxy data columns
     proxy_columns = ", ".join(list(data["proxy_data_columns"].iloc[0]))
     st.markdown(
         f'<div style="background-color: orange; padding: 10px; border-radius: 10px;">'
@@ -90,6 +102,7 @@ def visualize_single_data(data):
         unsafe_allow_html=True
     )
 
+    # Display missing data columns
     missing_columns = ", ".join(list(data["missing_data_columns"].iloc[0]))
     st.markdown(
         f'<div style="background-color: red; padding: 10px; border-radius: 10px;">'
@@ -100,5 +113,6 @@ def visualize_single_data(data):
     )
 
 
+# Entry point of the program
 if __name__ == "__main__":
-    main()
+    main()  # Call the main function
--- src/components/data_health_check.py
+++ src/components/data_health_check.py
@@ -1,99 +1,92 @@
 import pandas as pd
 
 
+# Function to get non-null column names for a row of Original Data
+def get_non_null_columns(row):
+    row = row[row['selected_columns']]
+    return set(row.index[row.notna()])
+
+
+# Function to get null column names for a row of Processed Data
+def get_null_columns(row):
+    row = row[row['selected_columns']]
+    return set(row.index[row.isna()])
+
+
+# Function to get proxy columns (columns with non-primary and non-missing data)
+def get_proxy_columns(row):
+    proxy_rows = set(row['selected_columns']) - row['primary_data_columns'] - row['missing_data_columns']
+    return proxy_rows
+
+
+# Class to perform data health check
 class DataHeathCheck:
     def __init__(self, data_path):
+        # Initialize class attributes and read data from sheets
         self.original_data = pd.read_excel(data_path, sheet_name="Original data")
         self.processed_data = pd.read_excel(data_path, sheet_name="Processed and completed data")
-        self.columns_to_exclude = ['DR code', 'GTIN', 'packaging_weight_unit', 'product_weight_unit_original',
-                                   'product_weight_unit', 'Material percentage check']
-        self.columns_to_include = [cols for cols in self.original_data.columns if cols not in self.columns_to_exclude]
-        self.count_product_material = sum(
-            col.startswith('material_') and col.endswith('_name') for col in self.processed_data.columns)
-        self.count_packaging_material = sum(
-            col.startswith('packaging_material_') and col.endswith('_characteristic') for col in self.processed_data.columns)
 
-    def select_columns(self, row):
-        columns_to_include = ['Name', 'Item Type', 'Brand', 'Product Weight', 'Packaging weight', 'Certification', 'Location of origin',
-                              'Location of destination', 'Transport mode']  # , 'COP']
-        for i in range(1, self.count_product_material + 1):
+    # Function to select relevant columns for processing based on row data
+    def _select_columns(self, row):
+        columns_to_include = ['Name', 'Item Type', 'Brand',
+                              'COP', 'Product Weight',
+                              'Packaging weight', 'Certification',
+                              'Location of origin', 'Location of destination', 'Transport mode']
+        # Count product and packaging material columns
+        count_product_material = sum(
+            col.startswith('material_') and col.endswith('_name') for col in self.processed_data.columns)
+        count_packaging_material = sum(
+            col.startswith('packaging_material_') and col.endswith('_characteristic') for col in
+            self.processed_data.columns)
+
+        # Include product material columns based on count
+        for i in range(1, count_product_material + 1):
             material_name_col = f'material_{i}_name'
             material_percentage_col = f'material_{i}_percentage'
             material_characteristic_col = f'material_{i}_characteristic'
             if not pd.isna(row[material_name_col]):
                 columns_to_include.extend([material_name_col, material_percentage_col, material_characteristic_col])
-        for j in range(1, self.count_packaging_material + 1):
+
+        # Include packaging material columns based on count
+        for j in range(1, count_packaging_material + 1):
             packaging_material = f'packaging_material_{j}_name'
             packaging_weight = f'packaging_material_{j}_weight'
             packaging_characteristic = f'packaging_material_{j}_characteristic'
-
             if not pd.isna(row[packaging_material]):
                 columns_to_include.extend([packaging_material, packaging_weight, packaging_characteristic])
-
         return columns_to_include
 
+    # Verify that original data GTIN values match processed data GTIN values
     def _verify_data(self):
         assert self.original_data['GTIN'].equals(self.processed_data['GTIN']), "Original Data GTIN values are not " \
                                                                                "same as processed values"
 
+    # Function to fetch the original and processed data
     def get_data(self):
         return self.original_data, self.processed_data
 
-    # # Define a function to get non-null column names for a row
-    # def get_non_null_columns(self, row):
-    #     return set(row.index[row.notna()])
-    #
-    # # Define a function to get non-null column names for a row
-    # def get_null_columns(self, row):
-    #     return set(row.index[row.isna()])
-
-    # Define a function to get non-null column names for a row
-    def get_null_columns(self, row):
-        row = row[row['selected_columns']]
-        return set(row.index[row.isna()])
-
-    # Define a function to get non-null column names for a row
-    def get_non_null_columns(self, row):
-        row = row[row['selected_columns']]
-        return set(row.index[row.notna()])
-
-    # Define a function to get non-null column names for a row
-    def get_proxy_columns(self, row):
-        proxy_rows = set(row['selected_columns']) - row['primary_data_columns'] - row['missing_data_columns']
-
-        return proxy_rows
-
+    # Calculate health check data
     def calculate_health_check(self):
-        # Verify if the original data and processed data have same GTIN values to process
+        # Verify if the original data and processed data have the same GTIN values to process
         self._verify_data()
 
         # Define the columns to be selected for each row based on Product and Packaging
-        self.processed_data["selected_columns"] = self.processed_data.apply(self.select_columns, axis=1)
-        self.original_data["selected_columns"] = self.processed_data.apply(self.select_columns, axis=1)
+        self.processed_data["selected_columns"] = self.processed_data.apply(self._select_columns, axis=1)
+        self.original_data["selected_columns"] = self.processed_data.apply(self._select_columns, axis=1)
 
-        self.processed_data['primary_data_columns'] = self.original_data.apply(self.get_non_null_columns, axis=1)
-        self.processed_data['missing_data_columns'] = self.processed_data.apply(self.get_null_columns, axis=1)
-        self.processed_data['proxy_data_columns'] = self.processed_data.apply(self.get_proxy_columns, axis=1)
+        # Calculate primary, missing, and proxy data columns
+        self.processed_data['primary_data_columns'] = self.original_data.apply(get_non_null_columns, axis=1)
+        self.processed_data['missing_data_columns'] = self.processed_data.apply(get_null_columns, axis=1)
+        self.processed_data['proxy_data_columns'] = self.processed_data.apply(get_proxy_columns, axis=1)
 
+        # Calculate counts for primary, missing, and proxy data columns
         self.processed_data['primary_data_count'] = self.processed_data['primary_data_columns'].apply(lambda x: len(x))
         self.processed_data['missing_data_count'] = self.processed_data['missing_data_columns'].apply(lambda x: len(x))
         self.processed_data['proxy_data_count'] = self.processed_data['proxy_data_columns'].apply(lambda x: len(x))
 
-        # self.processed_data['primary_data_count'] = self.original_data[self.columns_to_include].count(
-        #     axis=1)  # .apply(lambda x : x.count(), axis=1)
-        # self.processed_data['missing_data_count'] = self.processed_data[self.columns_to_include].isna().sum(
-        #     axis=1)  # .apply(lambda x : x.isna().sum(), axis=1)
-        # self.processed_data['proxy_data_count'] = len(self.columns_to_include) \
-        #                                           - self.processed_data['primary_data_count'] \
-        #                                           - self.processed_data['missing_data_count']
-        # # Apply the function to each row
-        # self.processed_data['primary_data_columns'] = self.original_data[self.columns_to_include].apply(self.get_non_null_columns, axis=1)
-        # self.processed_data['missing_data_columns'] = self.processed_data[self.columns_to_include].apply(self.get_null_columns, axis=1)
-        # self.processed_data['proxy_data_columns'] = set(self.columns_to_include) - self.processed_data[
-        #     'primary_data_columns'] - self.processed_data['missing_data_columns']
-
         return self.processed_data
 
+    # Create a comparison DataFrame for null value percentages
     def create_comparison_df(self):
         # Calculate the percentage of null values for both DataFrames
         self._verify_data()
@@ -102,10 +95,8 @@ class DataHeathCheck:
 
         # Create a new DataFrame to store the null value percentages
         comparison_df = pd.DataFrame({
-            # 'Columns': org_data_null_percent.index,
             'org_data_null_percent': org_data_null_percent,
             'processed_data_null_percent': processed_data_null_percent,
-            # 'total_null' : org_data_null_percent + processed_data_null_percent
         })
         comparison_df.fillna(0, inplace=True)
         comparison_df['total_null'] = comparison_df['org_data_null_percent'] + comparison_df[
